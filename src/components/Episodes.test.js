41e5e471160eb039eca1c22c6704281323cd40ee
Matthew Justice <matthewajustice@gmail.com>
2021-01-25 17:29:33 -0500

    Api call moved into api folder and app testing done, moving on to episode testing?

---
+++ src/components/Episodes.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import Episodes from './Episodes';
+
+test('Water is wet', async () => {
+  // render(<Episodes />);
+});
